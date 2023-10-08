0319bd17828fa5684e1b656f0ea1252a85ead6de
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-08 11:22:23 -0300

    Add politica de privacidade

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from dotenv import load_dotenv
 from openia import get_ia_response
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from whatsapp import extract_whatsapp_received_message, extract_whatsapp_received_number, send_whatsapp_message
 
 load_dotenv()
 
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.post('/whatsapp-business/webhook')
@@ -19,3 +24,8 @@ async def receive_whatsapp_webhook(request: Request):
         ia_response = get_ia_response(received_message)
 
         send_whatsapp_message(ia_response, received_number)
+
+
+@app.get('/politica-de-privacidade', response_class=HTMLResponse)
+def politica_de_privacidade():
+    return open("static/politica_de_privacidade.html").read()
